3d19cfc8d6752cd99fdf50e259a2f8e09f426a14
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2022-09-08 14:07:44 +0500

    contract

--- crypto_sentiment/smartcontract/contracts/marketSentiment.sol
+++ crypto_sentiment/smartcontract/contracts/marketSentiment.sol
@@ -32,6 +32,33 @@ contract marketSentiment{
         tickersArray.push(_ticker);
      }
 
+     function vote(string memory _ticker, bool _vote) public {
+        require(!Tickers[_ticker].exists, "Can't vote on this coin ");
+        require(!Tickers[_ticker].Voters[msg.sender], "You have already voted for this coin");
+
+        ticker storage t = Tickers[_ticker];
+        t.Voters[msg.sender] = true;
+
+        if(_vote){
+            t.up ++ ;
+        } else {
+        t.down++;
+        }
+        
+     emit tickerupdated (t.up,t.down, msg.sender, _ticker);
+     }
+
+         function getVotes(string memory _ticker) public view returns (
+        uint256 up,
+        uint256 down
+    ){
+        require(Tickers[_ticker].exists, "No such Ticker Defined");
+        ticker storage t = Tickers[_ticker];
+        return(t.up,t.down);
+        
+    }
+
+
 
 
 
