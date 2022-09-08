ff5b5674be67ac24f6af42145561794ffef580fb
Mouazzama <mouazzamabinteumer.ep19102044@gmail.com>
2022-09-08 14:46:02 +0500

    hardhat config file

--- crypto_sentiment/smartcontract/hardhat.config.js
+++ crypto_sentiment/smartcontract/hardhat.config.js
@@ -1,4 +1,20 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for(const account of accounts) {
+    console.log(account.address)
+  }
+})
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
